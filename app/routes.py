d37e25fe3f622cc198bd93f99b3138f4e21c6084
Deniska <deniskaost2005@gmail.com>
2024-07-16 22:05:31 +0300

    the commit

--- app/routes.py
+++ app/routes.py
@@ -1,114 +1,324 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from .models import User, Post, Comment
+from flask import Blueprint, request
+from flask_jwt_extended import create_access_token, get_jwt_identity, JWTManager, jwt_required, decode_token
+from .database import User, Post, Comment
+from .generate_captcha import generate_captcha, generate_captcha_image
+from .generate_token import encrypt_decrypt, generate_uuid, create_user_jwt_token
+from .validation_data import *
+from .server_exception import Response
+from .badwords_checker import BannedWordsChecker
+from dotenv import load_dotenv
+from functools import wraps
+import time
+import os
 
-api = Blueprint('api', __name__)
+load_dotenv()
 
+SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+TIME_CAPTCHA_LIMIT = int(os.getenv('JWT_EXPIRATION_MINUTES')) * 60000  # в миллисекундах
 
-@api.route('/auth/register/', methods=['POST'])
-def register():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    session_key = data.get('session_key')
-    email = data.get('email')
-    phone_number = data.get('phone_number')
-    pers_photo_data = data.get('pers_photo_data')
-
-    if User.find_by_email(email):
-        return jsonify({"msg": "User already exists"}), 400
-
-    user_id = User.create_user(username, password, first_name, last_name, session_key, email, phone_number,
-                               pers_photo_data)
-    return jsonify({"msg": "User registered successfully", "user_id": user_id}), 201
+api = Blueprint('api', __name__)  # добавляет api во всех раутах
+jwt = JWTManager()  # объект генерации токенов
 
 
-@api.route('/auth/login/', methods=['POST'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
-    user = User.find_by_email(email)
+# Задачи (не забыть блять):
 
-    if user and user['password'] == password:
-        access_token = create_access_token(identity=user['id'])
-        return jsonify(access_token=access_token), 200
-    else:
-        return jsonify({"msg": "Invalid credentials"}), 401
+# сделать эндпоинты для постов и комментов (ден и сергей)
+# протестить методы проверки постов и комментов
+# генерировать названия постов
+# волюм для sourses
 
 
-@api.route('/posts/', methods=['GET', 'POST'])
-@jwt_required()
+def token_required(f):  # метод проверки токенов авторизации
+    @wraps(f)
+    def decorator(*args, **kwargs):  # декоратор для проверки токена на каждом рауте
+        response = Response()
+
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split()[1]
+        if not token:
+            response.status(405)
+            return response.send()
+
+        try:
+            get_jwt_identity()  # будет ошибка, если токен недействителен
+        except:
+            response.set_status(406)
+            return response.send()
+
+        return f(*args, **kwargs)
+
+    return decorator
+
+
+@api.route('/captcha', methods=['GET'])  # метод генерации и получения капчи
+def get_captcha():
+    response = Response()
+
+    # генерация капчи
+    captcha_text = generate_captcha()
+    encoded_captcha_solution = encrypt_decrypt(captcha_text, SECRET_KEY)
+    base64_image = generate_captcha_image(captcha_text)
+    print("Текст капчи: " + captcha_text)
+    captcha_actual_time = int(time.time() * 1000) + TIME_CAPTCHA_LIMIT  # время, до которого капча валидна
+    token = create_access_token(identity={"solution": encoded_captcha_solution, "actual_time": captcha_actual_time})
+
+    response.set_data({
+        'captcha_image': base64_image,  # изображение капчи, закодированное в base64
+        'captcha_token': token  # закодированное решение капчи в виде jwt-токен
+    })
+    return response.send()
+
+
+@api.route('/auth', methods=['POST'])  # метод авторизации/регистрации пользователя
+def auth():
+    response = Response()
+    try:
+        # получаем значение Target-Action в заголовке запроса
+        action = request.headers.get('Target-Action')
+
+        data = request.get_json()
+        input_captcha = data.get("input_captcha")
+        captcha_solution_token = data.get("captcha_token")
+
+        # проверка наличия полей решения капчи
+        if not captcha_solution_token or not input_captcha:
+            response.set_status(411)
+            return response.send()
+
+        # декодируем капчу и время, до которого она валидна
+        try:
+            decoded_captcha_token = decode_token(captcha_solution_token)
+            captcha_solution = encrypt_decrypt(decoded_captcha_token['sub']['solution'], SECRET_KEY)
+            captcha_actual_time = decoded_captcha_token['sub']['actual_time']
+
+        except:
+            response.set_status(413)
+            return response.send()
+
+        current_time = int(time.time() * 1000)  # в миллисекундах
+
+        # проверка актуального времени капчи
+        if captcha_actual_time < current_time:
+            response.set_status(416)
+            return response.send()
+
+        # проверка пользовательского решения капчи
+        if input_captcha != captcha_solution:
+            response.set_status(414)
+            return response.send()
+
+        # логика регистрации
+        if action == 'REGISTER':
+            try:
+                # публичные поля (требуют проверки на badwords)
+                first_name = data.get('first_name')
+                middle_name = data.get('middle_name')
+                sur_name = data.get('sur_name')
+
+                # непубличные поля (не требуют проверки на badwords)
+                login = data.get('login')
+                password = data.get('password')
+                email = data.get('email')
+                phone_number = data.get('phone_number')
+                pers_photo_data = data.get('pers_photo_data')
+                header, pers_photo_data = pers_photo_data.split(",", 1)
+
+                # валидация все полей
+                is_valid, validation_error = check_user_data(data)
+                if not is_valid:
+                    response.set_status(417)
+                    response.set_message(validation_error)
+                    return response.send()
+
+                # проверка на плохие слова
+                first_name = BannedWordsChecker.bad_words(first_name)
+                middle_name = BannedWordsChecker.bad_words(middle_name)
+                sur_name = BannedWordsChecker.bad_words(sur_name)
+
+                # если false - проверка не пройдена
+                if not first_name or not middle_name or not sur_name:
+                    raise Exception("Inappropriate content")
+
+            except Exception as err:
+
+                if err == "Inappropriate content":
+                    response.set_status(418)
+                else:
+                    response.set_status(417)
+
+                return response.send()
+
+            # проверка и запись пользователя
+            try:
+                if User.find_by_login(login):
+                    response.set_status(409)
+                    return response.send()
+
+                # проверка иконки на наличие, валидность и "квадратность"
+                if (pers_photo_data is not None) and (not is_image_valid(pers_photo_data) or not is_icon_square(pers_photo_data)):
+                    response.set_status(420)
+                    return response.send()
+
+                unique_filename = generate_uuid()+".jpg"
+                pers_photo_data = save_icon(pers_photo_data, unique_filename)  # сохранение иконки и возврат ее пути для записи
+
+                # создание юзера в базе и выдача токена
+                User.create_user(login, password, first_name, middle_name, sur_name, email, phone_number, pers_photo_data)
+                unique_token = generate_uuid()
+                access_token = create_user_jwt_token(unique_token, login, password)
+
+                response.set_data({
+                    "session_token": access_token
+                })
+
+                return response.send()
+
+            # если ошибка в логике сервера
+            except Exception:
+                response.set_status(504)
+                return response.send()
+
+        # логика авторизации
+        elif action == 'LOGIN':
+            try:
+                login = data.get('login')
+                password = data.get('password')
+
+            except:
+                response.set_status(417)
+                return response.send()
+
+            # проверка пользователя
+            try:
+                user = User.find_by_login(login)
+
+            except Exception:
+                response.set_status(504)
+                return response.send()
+
+            if user and user['password'] == password:
+                unique_token = generate_uuid()
+                access_token = create_user_jwt_token(unique_token, login, password)
+
+                response.set_data({
+                    "session_key": access_token
+                })
+
+                return response.send()
+
+            else:
+                response.set_status(417)
+                return response.send()
+
+        else:
+            response.set_status(415)
+            return response.send()
+
+    # общая ошибка
+    except Exception:
+        response.set_status(400)
+        return response.send()
+
+
+
+
+@api.route('/posts/', methods=['GET'])
 def handle_posts():
-    if request.method == 'GET':
-        posts = Post.get_all_posts()
-        return jsonify(posts), 200
+    pass
 
-    if request.method == 'POST':
+@api.route('/post/create/', methods=['POST'])
+@jwt_required()
+def new_post():
+    response = Response()
+
+    try:
+        # Извлекаем информацию о пользователе из JWT токена
+        identity = get_jwt_identity()
+        user_login = identity['login']
+        user_password = identity['password']  # сохраняем также пароль для проверки
+
+        # Проверка существования пользователя и правильности пароля
+        user = User.find_by_login(user_login)
+        if not user or user['password'] != user_password:
+            response.set_status(401)
+            response.set_message('Invalid credentials.')
+            return response.send()
+
         data = request.get_json()
         title = data.get('title')
         content = data.get('content')
         tags = data.get('tags')
         image_data = data.get('image_data')
-        user_id = get_jwt_identity()
 
-        post_id = Post.create_post(user_id, title, content, tags, image_data)
-        return jsonify({"msg": "Post created successfully", "post_id": post_id}), 201
+        # Проверка наличия обязательных полей
+        if not title or not content:
+            response.set_status(417)
+            return response.send()
+
+        # Проверка валидности изображения
+        if image_data:
+            header, image_data = image_data.split(",", 1)
+            if not is_image_valid(image_data) or not is_icon_square(image_data):
+                response.set_status(420)
+                return response.send()
+
+        # Проверка на запрещённые слова в заголовке и содержании
+        title = BannedWordsChecker.bad_words(title)
+        content = BannedWordsChecker.bad_words(content)
+
+        if not title or not content:
+            response.set_status(418)
+            return response.send()
+
+        # Генерация уникального ID для поста
+        unique_post_id = generate_uuid()
+
+        # Создание нового поста в базе данных
+        try:
+            post_id = Post.create_post(unique_post_id, user_login, title, content, tags, image_data)
+            response.set_data({'post_id': post_id, 'unique_post_id': unique_post_id})
+            return response.send()
+
+        except Exception as e:
+            response.set_status(504)
+            return response.send()
+
+    except Exception as e:
+        response.set_status(400)
+        response.set_message('Bad request.')
+        return response.send()
 
 
-@api.route('/posts/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+
+@api.route('/post/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 @jwt_required()
 def handle_post(id):
-    post = Post.get_post_by_id(id)
+    pass
 
-    if request.method == 'GET':
-        return jsonify(post), 200
-
-    if request.method == 'PUT':
-        data = request.get_json()
-        title = data.get('title', post['title'])
-        content = data.get('content', post['content'])
-        tags = data.get('tags', post['tags'])
-        image_data = data.get('image_data', post['imageData'])
-        Post.update_post(id, title, content, tags, image_data)
-        return jsonify({"msg": "Post updated successfully"}), 200
-
-    if request.method == 'DELETE':
-        Post.delete_post(id)
-        return jsonify({"msg": "Post deleted successfully"}), 204
-
-
-@api.route('/posts/<int:post_id>/comments/', methods=['GET', 'POST'])
-@jwt_required()
+@api.route('/post/<int:post_id>/comments/', methods=['GET'])
 def handle_comments(post_id):
-    if request.method == 'GET':
-        comments = Comment.get_comments_by_post(post_id)
-        return jsonify(comments), 200
+    pass
 
-    if request.method == 'POST':
-        data = request.get_json()
-        content = data.get('content')
-        image_data = data.get('image_data')
-        tags = data.get('tags')
-        user_id = get_jwt_identity()
+@api.route('/post/<int:post_id>/comment/create/', methods=['POST'])
+@jwt_required()
+def new_comment():
+    pass
 
-        comment_id = Comment.create_comment(user_id, post_id, content, image_data, tags)
-        return jsonify({"msg": "Comment created successfully", "comment_id": comment_id}), 201
-
-
-@api.route('/comments/<int:id>/', methods=['PUT', 'DELETE'])
+@api.route('/post/<int:post_id>/comment/<int:id>/', methods=['PUT', 'DELETE'])
 @jwt_required()
 def handle_comment(id):
-    if request.method == 'PUT':
-        data = request.get_json()
-        content = data.get('content')
-        image_data = data.get('image_data')
-        tags = data.get('tags')
-        Comment.update_comment(id, content, image_data, tags)
-        return jsonify({"msg": "Comment updated successfully"}), 200
+    pass
 
-    if request.method == 'DELETE':
-        Comment.delete_comment(id)
-        return jsonify({"msg": "Comment deleted successfully"}), 204
+@api.route('/posts/<int:post_id>/view', methods=['PUT'])
+def update_view_count(post_id):
+    pass
+
+@api.route('/posts/<int:post_id>/like', methods=['PUT'])
+def update_likes_count(post_id):
+    pass
+
+@api.route('/posts/<int:post_id>/dislike', methods=['PUT'])
+def update_dislikes_count(post_id):
+    pass
+
