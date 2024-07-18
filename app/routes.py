8b9a8f222695003d9a2debc98871c7b99f00c0cb
Deniska <deniskaost2005@gmail.com>
2024-07-18 13:25:16 +0300

    edit_userprofile

--- app/routes.py
+++ app/routes.py
@@ -222,74 +222,95 @@ def auth():
 
 
 
+# стандартные значения
+# могут быть не указаны limit = 5 page = 1 orderByDate=desc, search=''
+# body = {
+#     'filters': {
+#         'orderByDate': 'desc',
+#         'search' : '',
+#     },
+#     'limit': 5,
+#     'page': 1,
+#     'totalPosts': 100,
+#     'posts': [],
+# }
 
-@api.route('/posts/', methods=['GET'])
+
+@api.route('/home', methods=['GET'])
 def handle_posts():
-    pass
+    response = Response()
+
+    # основной блок кода
+    try:
+
+        # получаем query string параметры для того, чтобы задать фильтры
+        limit = request.args.get('limit')
+        page = request.args.get('page')
+        order = request.args.get('orderByDate')
+        search = request.args.get('search')
+
+        # получаем число постов в базе по заданному фильтру search
+        posts_count = len(Post.get_all_posts(search=search))
+
+        # если остальные параметры заданы кринжово, устанавливаем на дефолтные
+        # а может, эти проверки нужно вынести в Post.get_all_posts()?
+        try:
+            limit = int(limit)
+            if limit > posts_count or limit < 1:
+                if posts_count < 5:
+                    limit = posts_count
+                else:
+                    limit = 5
+
+        except:
+            if posts_count < 5:
+                limit = posts_count
+            else:
+                limit = 5
+
+
+        try:
+            page = int(page)
+            if posts_count == limit or page > int(posts_count/limit) + 1 or page < 1:
+                page = 1
+
+        except:
+            page = 1
+
+
+        if order not in ['asc', 'desc']:
+            order = 'desc'
+
+
+        # пробуем получить и отправить посты
+        try:
+            posts = Post.get_all_posts(order, page, limit, search)
+            response.set_data({
+                'filters': {
+                    'orderByDate': order,
+                    'search' : search,
+                },
+                'limit': limit,
+                'page': page,
+                'totalPosts': posts_count,
+                'posts': posts,
+            })
+            return response.send()
+
+        except:
+            response.set_status(504) # Database error
+            return response.send()
+
+
+    except:
+        response.set_status(400) # Bad Request
+        return response.send()
+
 
 @api.route('/post/create/', methods=['POST'])
 @jwt_required()
 def new_post():
-    response = Response()
-
-    try:
-        # Извлекаем информацию о пользователе из JWT токена
-        identity = get_jwt_identity()
-        user_login = identity['login']
-        user_password = identity['password']  # сохраняем также пароль для проверки
-
-        # Проверка существования пользователя и правильности пароля
-        user = User.find_by_login(user_login)
-        if not user or user['password'] != user_password:
-            response.set_status(401)
-            response.set_message('Invalid credentials.')
-            return response.send()
-
-        data = request.get_json()
-        title = data.get('title')
-        content = data.get('content')
-        tags = data.get('tags')
-        image_data = data.get('image_data')
-
-        # Проверка наличия обязательных полей
-        if not title or not content:
-            response.set_status(417)
-            return response.send()
-
-        # Проверка валидности изображения
-        if image_data:
-            header, image_data = image_data.split(",", 1)
-            if not is_image_valid(image_data) or not is_icon_square(image_data):
-                response.set_status(420)
-                return response.send()
-
-        # Проверка на запрещённые слова в заголовке и содержании
-        title = BannedWordsChecker.bad_words(title)
-        content = BannedWordsChecker.bad_words(content)
-
-        if not title or not content:
-            response.set_status(418)
-            return response.send()
-
-        # Генерация уникального ID для поста
-        unique_post_id = generate_uuid()
-
-        # Создание нового поста в базе данных
-        try:
-            post_id = Post.create_post(unique_post_id, user_login, title, content, tags, image_data)
-            response.set_data({'post_id': post_id, 'unique_post_id': unique_post_id})
-            return response.send()
-
-        except Exception as e:
-            response.set_status(504)
-            return response.send()
-
-    except Exception as e:
-        response.set_status(400)
-        response.set_message('Bad request.')
-        return response.send()
-
-
+    pass
 
 @api.route('/post/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 @jwt_required()
@@ -322,3 +343,62 @@ def update_likes_count(post_id):
 def update_dislikes_count(post_id):
     pass
 
+@api.route('/edit_user', methods=['PUT'])
+def edit_userprofile():
+    response = Response()
+
+    try:
+        user_id = get_jwt_identity()  # Получаем идентификатор пользователя из JWT токена
+
+        # Получаем JSON данные из запроса
+        data = request.get_json()
+
+        # Поля для обновления
+        first_name = data.get('first_name')
+        middle_name = data.get('middle_name')
+        sur_name = data.get('sur_name')
+        password = data.get('password')
+        email = data.get('email')
+        phone_number = data.get('phone_number')
+        pers_photo_data = data.get('pers_photo_data')
+
+        # Валидация данных пользователя
+        is_valid, validation_error = check_user_data(data, updating=True)
+        if not is_valid:
+            response.set_status(417)
+            response.set_message(validation_error)
+            return response.send()
+
+        # Проверка на плохие слова
+        first_name = BannedWordsChecker.bad_words(first_name)
+        middle_name = BannedWordsChecker.bad_words(middle_name)
+        sur_name = BannedWordsChecker.bad_words(sur_name)
+
+        if not first_name or not middle_name or not sur_name:
+            response.set_status(418)
+            return response.send()
+
+        # Валидация и сохранение новой фотографии профиля
+        if pers_photo_data:
+            header, pers_photo_data = pers_photo_data.split(",", 1)
+            if not is_image_valid(pers_photo_data) or not is_icon_square(pers_photo_data):
+                response.set_status(420)
+                return response.send()
+
+            unique_filename = generate_uuid() + ".jpg"
+            pers_photo_data = save_icon(pers_photo_data, unique_filename)
+
+        # Обновление данных пользователя в базе данных
+        try:
+            User.update_user(user_id, first_name, middle_name, sur_name, password, email, phone_number, pers_photo_data)
+            response.set_status(200)
+            return response.send()
+        except Exception as e:
+            print(e)
+            response.set_status(504)
+            return response.send()
+
+    except Exception as e:
+        print(e)
+        response.set_status(400)
+        return response.send()
