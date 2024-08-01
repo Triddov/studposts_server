5bba16ecb007cc73150d2ce6c3dc8322f0b65f8e
serzh_frei <seruy906@gmail.com>
2024-08-01 23:22:51 +0300

    release 1.0

--- Vue/src/components/Posts/EditPost.vue
+++ Vue/src/components/Posts/EditPost.vue
@@ -9,13 +9,13 @@
                 <div class="title">
                     Придумайте название публикации <my-question>Введите название своей интереснейшей публикации</my-question>
                 </div>
-                <div class="input_block"><my-input placeholder="Какие места стоит посетить на отпуске ?" class="input-post" v-model="title" :limit="200"></my-input></div> 
+                <div class="input_block"><my-input placeholder="Какие места стоит посетить на отпуске ?" class="input-post" v-model="title" :limit="200"></my-input></div>
             </div>
             <div class="entercontent">
                 <div class="title">
-                    Добавьте содержание для публикации <my-question>Поделитесь с людьми своими мыслями в поле содержания</my-question>
+                    Добавьте содержание для публикации <my-question>Поделитесь своими мыслями!</my-question>
                 </div>
-                <div class="input_block"><my-textarea placeholder="Отпуск это прежде всего ответственная задача..." class="textarea-editpost" v-model="content"></my-textarea></div>
+                <div class="input_block"><my-textarea :limit="5000" placeholder="Отпуск - это прежде всего ответственная задача..." class="textarea-editpost" v-model="content"></my-textarea></div>
             </div>
             <div class="entertags">
                 <div class="title">
@@ -25,7 +25,7 @@
             </div>
             <div class="enterpicture">
                 <div class="title">
-                    Добавьте картинку для поста <my-question>Все любят красочные карртинки, добавьте одну, чтобы больше заинтересовать читателя</my-question>
+                    Добавьте картинку для поста <my-question>Все любят красочные картинки! Добавьте одну, чтобы больше заинтересовать читателя</my-question>
                 </div>
                 <div :class="{'input-editpost-picture-container' : !userPhoto, 'input-editpost-picture-container-ready' : userPhoto}">
                     <AddPhoto v-model="userPhoto" :isPost="true" v-show="!userPhoto"></AddPhoto>
@@ -46,6 +46,8 @@ import Footer from '../Parts/Footer.vue'
 import AddPhoto from '../Parts/AddPhoto.vue'
 import Photo from '../Parts/Photo.vue'
 import Info from '../Info/Info.vue'
+import MakeRequest from '../../API/Request.js'
+import { BASE_URL } from '../../BaseURL.js'
 
 export default {
     name: 'create-post',
@@ -55,7 +57,7 @@ export default {
         AddPhoto,
         Photo,
         Info
-    },  
+    },
     data()
     {
         return {
@@ -83,48 +85,63 @@ export default {
                 this.isError = true
                 return
             }
-            const response = await fetch(`${process.env.VUE_APP_BASE_URL}/api/updatePost/${this.$route.params.id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": document.cookie.split('session_token=')[1]	
-                },
-                body: JSON.stringify({
-                    title: this.title,
-                    content: this.content,
-                    tags: this.tags,
-                    image_data: this.userPhoto || null
-                })
-            })
-            if(!response)
+            const data = {
+                title: this.title,
+                content: this.content,
+                tags: this.tags,
+                image_data: this.userPhoto || null
+            }
+            if(/sources/.test(this.userPhoto))
             {
-                this.status_error = "Ошибка"
-                this.title_error = "Сервер недоступен"
-                this.isInfo = true
-                this.isError = true
+                data.image_data = "alreadyExist"
             }
-            if(/2../.test(String(response.status)))
-            {
-                this.$router.push('/home')
+            try{
+                const url = `${BASE_URL}/api/updatePost/${this.$route.params.id}`;
+                const params = {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": document.cookie.split('session_token=')[1]
+                    },
+                    body: JSON.stringify(data)
+                }
+
+                await MakeRequest(url, params);
+
+            //обработка ошибок
+            }catch(err){
+                this.title_error = err.message;
+                this.status_error = err.status;
+                this.isInfo = true;
+                this.isError = true;
+                return;
             }
-            else
-            {   
-                this.status_error = "Ошибка"
-                this.title_error = response.message
-                this.isInfo = true
-                this.isError = true
-            }
+
+            this.$router.push('/home')
         },
 
         async getPost()
         {
-            const response = await fetch(`${process.env.VUE_APP_BASE_URL}/api/post/${this.$route.params.id}`)
-            const data = await response.json()
-            this.post = data.post
+
+            let response;
+            try{
+                const url = `${BASE_URL}/api/post/${this.$route.params.id}`;
+                response = await MakeRequest(url);
+
+            //обработка ошибок
+            }catch(err){
+                this.title_error = err.message;
+                this.status_error = err.status;
+                this.isInfo = true;
+                this.isError = true;
+                return;
+            }
+
+            this.post = response.post
             this.title = this.post.title
             this.content = this.post.content
             this.tags = this.post.tags
-            this.userPhoto = `${process.env.VUE_APP_BASE_URL}/${this.post.imageData}` //QW
+            this.userPhoto = `${BASE_URL}/api/${this.post.imageData}` //QW
         }
     },
 
@@ -252,4 +269,3 @@ export default {
     }
 
 </style>
-    
